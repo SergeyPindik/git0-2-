f9dd6611dd5deadef5410e272f3ab2c19cfdc0d4
Notebook <Notebook@DESKTOP-5NLUEM6>
2024-05-12 14:23:59 +0700

    Добавьте файлы проекта.

---
+++ git0(2).cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    bool x = !(n % 2);
+    if (x) {
+        std::cout << "Yes\n";
+    }
+    else {
+        std::cout << "No\n";
+    }
+}
